3b15b71cf3c5e21b6358d6a55bc9f8eef8b2c6d6
Eunchurn Park <eunchurn.park@gmail.com>
2019-10-14 17:20:42 +0900

    no parser

--- slave/index.js
+++ slave/index.js
@@ -1,10 +1,8 @@
 const SerialPort = require("serialport");
-const Readline = require("@serialport/parser-readline");
 const port = new SerialPort("/dev/ttySAC3", {
   baudRate: Number(process.argv.slice(2))
 });
 
-const parser = new Readline();
-port.pipe(parser);
+port.on("data", console.log);
 
-parser.on("data", console.log);
+port.on("error", console.log)
