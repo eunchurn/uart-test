e633c7958aa56405f0294541bb3727375d22d52d
Eunchurn Park <eunchurn.park@gmail.com>
2019-10-15 12:52:44 +0900

    delimter

--- slave/index.js
+++ slave/index.js
@@ -1,8 +1,12 @@
 const SerialPort = require("serialport");
+const Delimiter = require("@serialport/parser-delimiter");
 const port = new SerialPort(process.argv[2], {
   baudRate: Number(process.argv[3])
 });
 
-port.on("data", console.log);
+const delimiter = Buffer.from("7D40", "hex");
+const parser = port.pipe(new Delimiter({ delimiter }));
 
-port.on("error", console.log)
+parser.on("data", console.log);
+
+parser.on("error", console.log);
