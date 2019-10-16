746480a526f2d8607677d9fbe3d83893d1940e44
Eunchurn Park <eunchurn.park@gmail.com>
2019-10-16 14:48:16 +0900

    no parser

--- slave/index.js
+++ slave/index.js
@@ -1,14 +1,16 @@
 const SerialPort = require("serialport");
-const Delimiter = require("@serialport/parser-delimiter");
+const fs = require("fs");
+// const Delimiter = require("@serialport/parser-delimiter");
 const port = new SerialPort(process.argv[2], {
   baudRate: Number(process.argv[3])
 });
 
-const delimiter = Buffer.from("7D40", "hex");
-const parser = port.pipe(new Delimiter({ delimiter }));
+// const delimiter = Buffer.from("7D40", "hex");
+// const parser = port.pipe(new Delimiter({ delimiter }));
 
-parser.on("data", data => {
+port.on("data", data => {
   console.log(`length: ${data.length}`, data.toString("hex"));
+  fs.writeFile("testdata.txt", data.toString("hex"), console.log);
 });
 
-parser.on("error", console.log);
+port.on("error", console.log);
